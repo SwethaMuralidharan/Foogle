79779abb9d7f3638ec97b10fc747320104b9ef42
Swetha Muralidharan <swethamuralidharan@Swethas-MacBook-Pro.local>
2017-12-20 11:14:44 -0800

    updated seed data

--- seed.js
+++ seed.js
@@ -5,7 +5,7 @@ var restaurants_list = [
     name: "Gary Danko",
     location: "Fisherman's Wharf",
     price_range: "Expensive",
-    serviceTime: "30 min"
+    serviceTime: "30 min",
     cuisine: "American" ,
     OperationHours: [ "Mon-fri : 5:30pm to 10pm" ]
     },
@@ -13,7 +13,7 @@ var restaurants_list = [
     name: "Velvet Cantina",
     location: "Mission District",
     price_range: "moderate",
-    serviceTime: "30 min"
+    serviceTime: "30 min",
     cuisine: "Mexican" ,
     OperationHours: [ "Mon-fri : 5 pm to 12am" ]
    },
@@ -21,7 +21,7 @@ var restaurants_list = [
     name: "Chez Maman",
     location: "Potrero Hill",
     price_range: "moderate",
-    serviceTime: "45 min"
+    serviceTime: "45 min",
     cuisine: "French",
     OperationHours: [ "Mon-Fri : 11:30am to 11pm", "Sat-Sun : 10:30am to 11pm" ]
    },
@@ -29,7 +29,7 @@ var restaurants_list = [
     name: "Villa Taverna",
     location: "Financial District",
     price_range: "expensive",
-    serviceTime: "30 min"
+    serviceTime: "30 min",
     cuisine: "Italian",
     OperationHours: [ "Mon-Fri : 11:30am to 2pm" ]
     },
@@ -37,7 +37,7 @@ var restaurants_list = [
     name: "Omakase",
     location: "Mission Bay",
     price_range: "expensive",
-    serviceTime: "25 min"
+    serviceTime: "25 min",
     cuisine: "Japanese",
     OperationHours: [ "Mon-Sun : 5pm to 9:30pm" ]
     },
@@ -45,7 +45,7 @@ var restaurants_list = [
     name: "Udupi Palace",
     location: "Mission",
     price_range: "moderate",
-    serviceTime: "10 min"
+    serviceTime: "10 min",
     cuisine: "Indian",
     OperationHours: [ "Sun-Thurs : 11:30am to 10pm", "Fri-Sat : 11:30am to 10:30pm" ]
     },
@@ -53,7 +53,7 @@ var restaurants_list = [
     name: "Hakkasan",
     location: "Union Square",
     price_range: "moderate",
-    serviceTime: "45 min"
+    serviceTime: "45 min",
     cuisine: "Chinese",
     OperationHours: [ "Mon-Sat : 11:30am to 10pm", "Sun : Closed" ]
     },
@@ -61,7 +61,7 @@ var restaurants_list = [
     name: "Pica Pica Arepa Kitchen",
     location: "Mission",
     price_range: "moderate",
-    serviceTime: "15 min"
+    serviceTime: "15 min",
     cuisine: "Peruvian",
     OperationHours: [ "Sun-Thur : 11am to 9pm",  "Fri-Sat : 11am to 10pm" ]
     },
@@ -69,7 +69,7 @@ var restaurants_list = [
     name: "Piccadilly Fish & Chips",
     location: "Nob Hill",
     price_range: "cheap",
-    serviceTime: "10 min"
+    serviceTime: "10 min",
     cuisine: "English",
     OperationHours: [ "Mon-Sun : 11am to 11pm" ]
     },
@@ -77,7 +77,7 @@ var restaurants_list = [
     name: "Pastel Brazzuca",
     location: "Potrero Hill",
     price_range: "cheap",
-    serviceTime: "10 min"
+    serviceTime: "10 min",
     cuisine: "Brazillian",
     OperationHours: [ "Mon-Fri : 8am to 7pm", "Sat: 9am to 2:30pm" ]
     }
